d9805ada942c6be2b536a706d826dbc209a457ac
Vox Humana <jonprine@gmail.com>
2020-07-28 17:59:20 -0500

    added page-tempalte.js

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-var commandLineArgs = process.argv;
-console.log(commandLineArgs);
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+//   console.log('==============');
+
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// };
+
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-  console.log('==============');
 
-  profileDataArr.forEach(profileItem => console.log(profileItem));
 
-};
-
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
