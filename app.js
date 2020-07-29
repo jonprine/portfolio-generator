a789ddecf3241790a5a2878705228e063740d21d
Vox Humana <jonprine@gmail.com>
2020-07-29 15:41:04 -0500

    added inquirer package and implemented question

--- app.js
+++ app.js
@@ -14,17 +14,28 @@
 
 // };
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+// const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
